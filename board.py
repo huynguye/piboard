caf393e1a30e88cc68781ac8a3248b1d18b460a0
Huy Nguyen <huynguye@gmail.com>
2016-04-27 12:45:50 -0700

    Slight update in sound files.

--- board.py
+++ board.py
@@ -5,10 +5,17 @@ import curses
 pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
 
-up = pygame.mixer.Sound('up.wav')
-down = pygame.mixer.Sound('down.wav')
-left = pygame.mixer.Sound('left.wav')
-right = pygame.mixer.Sound('right.wav')
+up    = pygame.mixer.Sound('beats/clap.wav')
+down  = pygame.mixer.Sound('beats/closed.wav')
+left  = pygame.mixer.Sound('beats/cymbal.wav')
+right = pygame.mixer.Sound('beats/kick.wav')
+
+s_clap   = pygame.mixer.Sound('beats/clap.wav')
+s_closed = pygame.mixer.Sound('beats/closed.wav')
+s_cymbal = pygame.mixer.Sound('beats/cymbal.wav')
+s_kick   = pygame.mixer.Sound('beats/kick.wav')
+s_open   = pygame.mixer.Sound('beats/open.wav')
+s_snare  = pygame.mixer.Sound('beats/snare.wav')
 
 screen = curses.initscr()
 curses.noecho()
@@ -22,28 +29,28 @@ while True:
 	screen.addstr(2, 0, "To get to a new console press Alt-F2")
 
 	event = screen.getch()
-
 	screen.clear()
 
 	if event == ord('q'):
 		break
 	elif event == curses.KEY_UP:
-		screen.addstr(0, 0, "You pressed up!")
+		# screen.addstr(0, 0, "You pressed up!")
 		up.play()
 	elif event == curses.KEY_DOWN:
-		screen.clear()
-		screen.addstr(0, 0, "You pressed down!")
+		# screen.clear()
+		# screen.addstr(0, 0, "You pressed down!")
 		down.play()
 	elif event == curses.KEY_LEFT:
-		screen.clear()
-		screen.addstr(0, 0, "You pressed left!")
+		# screen.clear()
+		# screen.addstr(0, 0, "You pressed left!")
 		left.play()
 	elif event == curses.KEY_RIGHT:
-		screen.clear()
-		screen.addstr(0, 0, "You pressed right!")
+		# screen.clear()
+		# screen.addstr(0, 0, "You pressed right!")
 		right.play()
 	else:
-		screen.clear()
-		screen.addstr(0, 0, "That key doesn't do anything!")
+		# screen.clear()
+		# screen.addstr(0, 0, "That key doesn't do anything!")
+		pass
 
 curses.endwin()
